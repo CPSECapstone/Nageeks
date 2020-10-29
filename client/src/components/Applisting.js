ff54c6253b87724f3853b7bb0efb150fef99a94d
bbdunning <40685582+bbdunning@users.noreply.github.com>
2020-10-28 19:52:18 -0700

    page navigation works

---
+++ client/src/components/Applisting.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+export default function Sidebar (props) {
+    return (
+        <p></p> 
+    );
+}
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,19 +1,37 @@
-import React, {useState, setState} from 'react';
+import React, {useState} from 'react';
 import "./Sidebar.css";
 
 export default function Sidebar (props) {
+  const[sidebarNum, setSidebarNum] = useState(0)
 
   function openContent(e, id) {
     console.log('I was clicked');
     props.cb(id);
+    setSidebarNum(id);
   }
 
   return (
-    <section className="side-bar">
-      <button onClick={(e) => openContent(e, 0)}>Apps</button>
-      <button className="tablinks" onClick={(e) => openContent(e, 1)}>My Data</button>
-      <button className="tablinks" onClick={(e) => openContent(e, 2)}>Calendar</button>
+    <div className="side-bar">
+      
+      <button 
+        className= {sidebarNum === 0 ? "side-bar-active" : "side-bar-button"}
+        onClick={(e) => openContent(e, 0)}
+      >
+      Apps
+      </button>
+      <button 
+        className= {sidebarNum === 1 ? "side-bar-active" : "side-bar-button"}
+        onClick={(e) => openContent(e, 1)}
+      >
+        My Data
+      </button>
+      <button 
+        className= {sidebarNum === 2 ? "side-bar-active" : "side-bar-button"}
+        onClick={(e) => openContent(e, 2)}
+      >
+        Calendar
+      </button>
       <div className="sidebar-fill"></div>
-    </section>
+    </div>
   );
 };
