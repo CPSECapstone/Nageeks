3c2f06777b4d066884557b3a562f1e4b0e2804b7
bbdunning <40685582+bbdunning@users.noreply.github.com>
2020-10-28 18:02:35 -0700

    properly setting app state from menubar

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, {useState, setState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
 import Sidebar from './components/Sidebar';
 import Banner from './components/Banner';
 
+
 function App() {
+  const [pageNum, setPageNum] = useState(0)
+
+  // const incrementer = () => {
+  //   setPageNum(pageNum + 1);
+  // }
+  
+  function incrementer(i) {
+    setPageNum(i);
+  }
+
   return (
     <div className="App">
-      <header><Banner/></header>
-      <Sidebar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Test 
-        </a>
-        <Customers />
-      </header>
+      <Banner/>
+      <p>{pageNum}</p>
+      <div className="home">
+        <Sidebar cb={incrementer}/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Test 
+          </a>
+          <script>
+          if (pageNum === 0) {
+            <Customers />
+          }
+          console.log(pageNum);
+          </script>
+        </header>
+      </div>
     </div>
   );
 }
