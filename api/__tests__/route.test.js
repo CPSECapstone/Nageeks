1e59ca1e3f33637a748a1fc1a1f323a45ed7c650
Harmless19 <43099759+Harmless19@users.noreply.github.com>
2020-11-05 14:54:50 -0800

    dotenv is annoying and does not have an override feature, so the NODE_ENV when running jest will always be 'test'

--- api/__tests__/route.test.js
+++ api/__tests__/route.test.js
@@ -1,8 +1,9 @@
-const request = require("supertest");
 const app = require("../app");
+const request = require("supertest");
 const mongoose = require('mongoose');
 
 describe("GET /customers ", () => {
+
     test("Respond with an array of customers", async () => {
         const expected = [{"_id":"5f97cbaa5da6005906710cb9","username":"John"},{"_id":"5f97cbc75da6005906710cba","username":"Jane"}];
         const response = await request(app).get("/customers");
--- api/config/keys.js
+++ api/config/keys.js
@@ -2,8 +2,11 @@ if (process.env.NODE_ENV === 'production'){
     console.log('Exporting production mongoURI');
     module.exports = require('./keys_prod');
 }
-else{
+else if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV == 'test'){
     console.log('Exporting dev mongoURI');
     console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
     module.exports = require('./keys_dev');
+}
+else{
+    console.error('Failed to export keys');
 }
