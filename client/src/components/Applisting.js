4ea677261c5b29cfbc886726d73d671a744cdf0e
bbdunning <40685582+bbdunning@users.noreply.github.com>
2020-11-08 16:27:11 -0800

    reworked app cards and added icons

--- client/src/components/Applisting.js
+++ client/src/components/Applisting.js
@@ -1,62 +1,39 @@
 import React, {useState} from 'react';
-import { Card, Grid } from '@material-ui/core'
+import { Card, CardContent, CardHeader, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import "./Applisting.css"
+import AppCard from './AppCard';
 
 const useStyles = makeStyles({
-  sidebarStyle: {
-    backgroundColor: "#323236"
+  containerStyle: {
+    padding: "10px",
   },
-  mainContentStyle: {
-    color: "whitesmoke",
-    backgroundColor: "#444449",
-    fontSize: "18px",
+  cardStyle: {
   },
 })
 
 export default function Applistings(props) {
+  const classes = useStyles();
+
   return (
     <Grid 
       container
-      spacing={3}
+      className={classes.containerStyle}
+      justify="space-evenly"
+      alignItems="flex-start"
     >
-      <Grid item xs={3}>
-        <Card
-          
-        >
-          hi
-        </Card>
+      <Grid item xs={2} >
+        <AppCard />
       </Grid>
-      <Grid item xs={3}>
-        <Card>
-          hi
-        </Card>
+      <Grid item xs={2}>
+        <AppCard/>
       </Grid>
-      <Grid item xs={3}>
-        <Card>
-          hi
-        </Card>
+      <Grid item xs={2}>
+        <AppCard/>
       </Grid>
-      <Grid item xs={3}>
-        <Card>
-          hi
-        </Card>
+      <Grid item xs={2}>
+        <AppCard/>
       </Grid>
     </Grid>
-      // <div className="applist">
-      //   <div className="applisting-box">
-      //     <img className="app-pic" src="https://npuh82iut7x3aosxba3ol14m-wpengine.netdna-ssl.com/wp-content/uploads/2018/11/ucdavismedical-740x675.jpg" alt="UC Davis Medical" width="130" height="130"/>
-      //     <p className="app-title-text"> UC Davis Medical Portal</p>
-      //   </div>
-      //   <div className="applisting-box">
-
-      //   </div>
-      //   <div className="applisting-box">
-
-      //   </div>
-      //   <div className="applisting-box">
-
-      //   </div>
-      // </div>
     );
 }
