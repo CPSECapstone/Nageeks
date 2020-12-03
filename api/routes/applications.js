7d731041e969dd74d77e5033771005e9df6d70d0
Garret Poole <55676259+garretpoole@users.noreply.github.com>
2020-12-02 16:19:19 -0800

    getting app api working

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
-const User = require('../models/user');
-const mongoose = require('../mongoose_connection');
-const uri = "http://api.cloudhaven.com"
+var axios = require('axios');
 
-router.route('/UCDavis')
+//http
+//store collection of uris in DB or file that gets updates
+
+router.route('/UCDavis/forms/a')
     // get form from thirdparty application
     .get(async function(req, res, next) {
         try{
-            // documents are stored as BSON so thats what we get here
-            const users = await User.find(); 
-            // res.json will convert the bson to json and send that in the response
-            res.status(200).json(users);
+            // get axios data from the third party application
+            axios.get('')
+            // res.json will convert the get from axios to json and send that in the response
+            res.status(200).json();
         }
         catch (err){
             console.error(err.message);
             res.status(404).json({message: "Error 404: User collection not found"});
         }
     })
+
+module.exports = router;
