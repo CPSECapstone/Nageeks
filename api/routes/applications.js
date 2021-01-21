2478a9276efc2520f4ffd369adbbc2992ef01ba2
Garret Poole <55676259+garretpoole@users.noreply.github.com>
2021-01-20 22:04:56 -0800

    routes are hardcoded jsons

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -7,32 +7,17 @@ var axios = require('axios');
 //store collection of uris in DB or file that gets updates
 
 router.route('/UCDavis/forms/:formId')
-    // get form from thirdparty application
+    // get form from hardcoded jsons
     .get(async function(req, res, next) {
         const formId = req.params.formId;
-        var url = ''
-        if(formId === 'a') {
-            url = 'http://localhost:3002/cloudhaven/forms/a';
+        if(formId === "a") {
+            return res.status(200).json({firstName: 'textfield', lastName: 'textfield', dob: 'textfield', phoneNumber: 'textfield'});
         }
-        else if(formId === 'b') {
-            url = 'http://localhost:3002/cloudhaven/forms/b';
+        else if(formId === "b") {
+            return res.status(200).json({country: 'textfield', firstLine: 'textfield', secondLine: 'textfield', city: 'textfield', state: 'textfield', zipcode: 'textfield'});
         }
         else {
-            res.status(404).json({message: "Invalid URL"})
-            res.end();
-        }
-        try{
-            // get axios data from the third party application
-            var formData = (await axios.get(url));
-            return res.status(200).json(formData.data)
-        }
-        catch (err){
-            console.error("Error Response:")
-            console.error(err.response.data);
-            console.error(err.response.status);
-            console.error(err.response.headers);
-            //res.status(404).json({message: "Error 404: Form not found"});
-            res.status(404).json({message: err.response});
+            return res.status(404).json({message: 'Form not available', formid: formId})
         }
     })
 
