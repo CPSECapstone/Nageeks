4cdc9c79af46cedc1f1930e421b958fbe161ce3a
Garret Poole <55676259+garretpoole@users.noreply.github.com>
2021-02-18 14:27:06 -0800

    changed jsons to const - code review

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -2,7 +2,10 @@ var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
 var axios = require('axios');
-
+//added after Code Review
+const formA = {firstName: 'textfield', lastName: 'textfield', dob: 'textfield', phoneNumber: 'textfield'};
+const formB = {country: 'textfield', firstLine: 'textfield', secondLine: 'textfield', city: 'textfield', state: 'textfield', zipcode: 'textfield'};
+const formEmplA = {Client: 'textfield', city: 'textfield', state: 'textfield', zipcode: 'textfield'};
 //http
 //store collection of uris in DB or file that gets updates
 
@@ -11,15 +14,15 @@ router.route('/UCDavis/forms/:formId')
     .get(async function(req, res, next) {
         const formId = req.params.formId;
         if(formId === "a") {
-            return res.status(200).json({firstName: 'textfield', lastName: 'textfield', dob: 'textfield', phoneNumber: 'textfield'});
+            return res.status(200).json(formA);
         }
         else if(formId === "b") {
-            return res.status(200).json({country: 'textfield', firstLine: 'textfield', secondLine: 'textfield', city: 'textfield', state: 'textfield', zipcode: 'textfield'});
+            return res.status(200).json(formB);
         }
         //TODO routing to an actual 3rd party would take care of what forms show up but need to find a way to 
         //tokenize user to send to third party app
         else if(formId === "emplA") {
-            return res.status(200).json({Client: 'textfield', city: 'textfield', state: 'textfield', zipcode: 'textfield'});
+            return res.status(200).json(formEmplA);
         }
         else {
             return res.status(404).json({message: 'Form not available', formid: formId})
